beb023467bcc1bc4a3577c9852c31e056a28c88b
root <root@DESKTOP-7CO9QQK>
2023-05-16 17:46:32 +0200

    starting the project

--- Libft/all_libft.h
+++ Libft/all_libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   all_libft.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 16:40:12 by lgabet            #+#    #+#             */
+/*   Updated: 2023/05/16 16:40:20 by lgabet           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ALL_LIBFT_H
 # define ALL_LIBFT_H
 
@@ -5,4 +17,4 @@
 # include "libft/includes/libft.h"
 # include "printf/includes/ft_printf.h"
 
-# endif
+#endif
--- src/main.c
+++ src/main.c
@@ -6,13 +6,18 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:38:46 by lgabet            #+#    #+#             */
-/*   Updated: 2023/05/16 15:39:55 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/05/16 17:43:57 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
-int	main()
+int	main(int ac, char **av)
 {
-	ft_printf("salut\n");
+	if (ac != 5)
+	{
+		ft_printf("Error\nWrong number of argument\n");
+		return (0);
+	}
+	
 }
