17682edd9236c42c9514d458149cd2a0456722fc
Lucien Gabet <lgabet@z4r10p7.42lyon.fr>
2023-05-24 14:09:35 +0200

    get the result of the cmd in tamp

--- src/ft_apply.c
+++ src/ft_apply.c
@@ -6,7 +6,7 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:54:50 by lgabet            #+#    #+#             */
-/*   Updated: 2023/05/24 12:44:30 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/05/24 14:08:54 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,30 +41,53 @@ int	ft_apply_cmd_1(t_com *com, char **av, char **env)
 	exe_arg[i] = av[1];
 	i++;
 	exe_arg[i] = NULL;
+	dup2(com->fd[1], 1);
 	execve(exe_arg[0], exe_arg, env);
 	return (0);
 }
 
+char	*ft_read_pipex(int fd)
+{
+	char	*tamp;
+	char	*str;
+
+	str = malloc(sizeof(char));
+	if (!str)
+		return (ft_printf("Error with malloc ft_read 1\n"), NULL);
+	str[0] = 0;
+	tamp = get_next_line(fd);
+	while (1)
+	{
+		str = ft_strjoin(str, tamp);
+		tamp = get_next_line(fd);
+		if (!str)
+			return (ft_printf("Error with malloc ft_read 2\n"), NULL);
+		if (!tamp)
+			break ;
+	}
+	return (str);
+		
+}
+
 int	ft_apply_cmd(t_com *com, char **av, char **env)
 {
-	char	tamp[200];
+	char	*tamp;
 	int		id;
 
-	// tamp = NULL;
 	if (pipe(com->fd))
 		return (ft_printf("Error with pipe\n"), 1);
 	id = fork();
 	if (id == 0)
-	{
-		// ft_printf("salut\n");
 		ft_apply_cmd_1(com, av, env);
-	}
 	else
 	{
 		close(com->fd[1]);
 		waitpid(id, 0, 0);
-		ft_printf("size read = %d\n", read(com->fd[0], tamp, 200));
-		ft_printf("read from pipe: %s\n", tamp);
+		// ft_printf("read = %d\n", read(com->fd[0], 0, 0));
+		tamp = ft_read_pipex(com->fd[0]);
+		if (!tamp)
+			return (ft_printf("Error with tamp ft_apply_cmd\n"), 1);
+		ft_printf("%s\n", tamp);
 	}
 	return (0);
 }
