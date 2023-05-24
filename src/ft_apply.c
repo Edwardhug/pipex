b09202b41abd71f8b19a03ff6e13c89b98053846
Lucien Gabet <lgabet@z4r10p7.42lyon.fr>
2023-05-24 12:44:52 +0200

    can add arg to cmd

--- src/ft_apply.c
+++ src/ft_apply.c
@@ -6,23 +6,41 @@
 /*   By: lgabet <lgabet@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 14:54:50 by lgabet            #+#    #+#             */
-/*   Updated: 2023/05/24 11:40:06 by lgabet           ###   ########.fr       */
+/*   Updated: 2023/05/24 12:44:30 by lgabet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
+int	ft_strlen_tab(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+		i++;
+	return (i);
+}
+
 int	ft_apply_cmd_1(t_com *com, char **av, char **env)
 {
-	char	*exe_arg[3];
+	char	**exe_arg;
+	int		i;
 
 	close(com->fd[0]);
-	ft_printf("com->fd[1] = %d\n", com->fd[1]);
-	ft_printf("dup2 = %d\n", dup2(com->fd[1], 1));
-	// ft_printf("salut\n");
+	exe_arg = malloc(sizeof(char *) * (ft_strlen_tab(com->cmd1) + 2));
+	if (!exe_arg)
+		return (ft_printf("Error malloc exe_arg\n"), 1);
 	exe_arg[0] = com->path_cmd1;
-	exe_arg[1] = av[1];
-	exe_arg[2] = NULL;
+	i = 1;
+	while (com->cmd1[i])
+	{
+		exe_arg[i] = com->cmd1[i];
+		i++;
+	}
+	exe_arg[i] = av[1];
+	i++;
+	exe_arg[i] = NULL;
 	execve(exe_arg[0], exe_arg, env);
 	return (0);
 }
@@ -43,7 +61,7 @@ int	ft_apply_cmd(t_com *com, char **av, char **env)
 	}
 	else
 	{
-		// close(com->fd[1]);
+		close(com->fd[1]);
 		waitpid(id, 0, 0);
 		ft_printf("size read = %d\n", read(com->fd[0], tamp, 200));
 		ft_printf("read from pipe: %s\n", tamp);
